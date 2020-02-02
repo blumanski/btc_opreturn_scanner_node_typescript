d22e1b16ba8e2bab75aa1ecbc415dd722a514f06
blumanski <blumanski@protonmail.com>
2020-02-02 16:52:46 +1000

    updated queue and removed useless code. still nothing stable to usewq

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
 import { OpScanner } from './opscanner';
 const OpScannerClient = new OpScanner();
 
-OpScannerClient.queueScanner(390076, 1, true);
+OpScannerClient.queueScanner(390076, 391076);
--- src/app/opscanner.ts
+++ src/app/opscanner.ts
@@ -9,14 +9,12 @@
 
 declare function require(name: string);
 const Buffer = require('buffer').Buffer;
-
 const bitcoreLib = require('bitcore-lib');
 const RpcClient = require('bitcoind-rpc');
 const zlib = require('zlib');
-const  yauzl = require("yauzl");
+const yauzl = require("yauzl");
 const punycode = require('punycode');
-const {gzip, ungzip} = require('node-gzip');
-//const pako = require('pako');
+const { gzip, ungzip } = require('node-gzip');
 
 // model for rpc response
 import { RpcResponse } from './models/rpcresponse.model';
@@ -25,16 +23,14 @@ export class OpScanner {
 
   private config = {
     protocol: 'http',
-    user: 'XXXX',
-    pass: 'XXXXXX',
+    user: '***',
+    pass: '****',
     host: '127.0.0.1',
     port: '8332',
   };
 
   private rpc: any;
-  private startHeight: number = 390076;
-  private defaultLimit: number = 2;
-  private CurrentTargetHeight: number = 0;
+  private defaultLimit: number = 30;
 
   constructor() {
     this.rpc = new RpcClient(this.config);
@@ -48,43 +44,38 @@ export class OpScanner {
    * @param limit
    * @param boolean 
    */
-  public queueScanner(start?: number, limit?: number, setMax?: boolean) {
-    // use default if none given
-    if (!start) {
-      start = this.startHeight;
-    }
-    // use default if none diven
-    if (!limit) {
-      limit = this.defaultLimit;
-    }
+  public queueScanner(start: number, max: number) {
 
-    // At the first call (from outside), it will set the target max range
-    if(setMax === true) {
-      this.CurrentTargetHeight = (start + limit);
-    } else {
-      if(this.CurrentTargetHeight < (start + limit)) {
-        //console.log('Completed');
-        return 'Complete';
-      }
-    }
+    this.getblockcount()
+      .then(maxHeight => {
+        if (maxHeight < max) {
+          console.log('end of chain reached...');
+          return;
+        }
 
-    // have a bit delay to avoid overcrowding the json-rpc
-    (async () => { 
-        
-        await this.delay(300)
-          .then(any => {
-            this.scanForMessages(start, limit)
-              .then(any => {
-                this.queueScanner((start + limit), limit);
-              })
-          })
+        if (max < (start)) {
+          //console.log('Completed');
+          return 'Complete';
+        }
 
-    })();
+        // have a bit delay to avoid overcrowding the json-rpc
+        (async () => {
+          await this.delay(300)
+            .then(any => {
+              this.scanForMessages(start, this.defaultLimit)
+                .then(any => {
+                  this.queueScanner((start + this.defaultLimit), max);
+                })
+            })
+
+        })();
+
+      })
 
   }
 
   private delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   /**
@@ -94,126 +85,143 @@ export class OpScanner {
   private scanForMessages(start: number, limit: number) {
 
     return new Promise((resolve, reject) => {
-          // declare array that will hold all op_returns
-          let opReturns = [];
-          // start looping through blocks 
-          for (let i = start; i <= (start + limit); i++) {
-            // start next promise promise chain
-            // get block hash
-            let a = this.getblockhash(i);
-            // get block using the hash
-            let b = a.then((hash: string) => {
-              return this.getblock(hash);
-            });
+      // declare array that will hold all op_returns
+      let opReturns = [];
+      // start looping through blocks 
+      for (let i = start; i <= (start + limit); i++) {
+        // start next promise promise chain
+        // get block hash
+        let a = this.getblockhash(i);
+        // get block using the hash
+        let b = a.then((hash: string) => {
+          return this.getblock(hash);
+        });
 
-            Promise.all([a, b])
-              .then((subs: Array<any>) => {
-                // test if array keys exist
-                if (subs[0] && subs[1]) {
+        Promise.all([a, b])
+          .then((subs: Array<any>) => {
+            // test if array keys exist
+            if (subs[0] && subs[1]) {
 
-                  // console.log('blockhash -> ', subs[0]);
-                  // console.log('block -> ', subs[1]);
-                  // console.log("\n-----------End first step ----------------\n")
-                  
-                  // test if array keys exist and not empty
-                  if (subs[1]['tx'] && subs[1]['tx'].length > 0) {
-                    // foreach through the the array
-                    subs[1]['tx'].forEach((value: object, index: number) => {
+              //console.log('blockhash -> ', subs[0]);
+              // console.log('block -> ', subs[1]);
+              // console.log("\n-----------End first step ----------------\n")
 
-                      // console.log('tx '+index+' -> ', value);
-                      // console.log("\n...............End second step ................\n")
+              // test if array keys exist and not empty
+              if (subs[1]['tx'] && subs[1]['tx'].length > 0) {
+                // foreach through the the array
+                subs[1]['tx'].forEach((value: object, index: number) => {
 
-                      // get raw transaction
-                      this.getrawtransaction(value)
-                        .then((result: RpcResponse) => {
+                  // console.log('tx '+index+' -> ', value);
+                  // console.log("\n...............End second step ................\n")
 
-                          // console.log('version -> ', result['version'])
-                          // console.log('Raw Transaction -> ', result);
-                          // console.log("\n...............End third step ................\n")
+                  // get raw transaction
+                  this.getrawtransaction(value)
+                    .then((result: RpcResponse) => {
 
-                          if(result['vout'] && result['vout'].length > 0) {
-                            // @todo
-                            // needs testing, see if this is always key "vout" or other may other keys too
-                            // loop through vout array
-                            let concatOpReturns: Array<any> = new Array();
-                            let texid:any;
+                      // console.log('version -> ', result['version'])
+                      // console.log('Raw Transaction -> ', result);
+                      // console.log("\n...............End third step ................\n")
 
-                            result['vout'].forEach((vout: object | null) => {
+                      if (result['vout'] && result['vout'].length > 0) {
+                        // @todo
+                        // needs testing, see if this is always key "vout" or other may other keys too
+                        // loop through vout array
+                        let concatOpReturns: Array<any> = new Array();
+                        let texid: any;
 
-                              if(vout.hasOwnProperty('value') && vout['value'] == 0) {
-                               //console.log(vout)
+                        result['vout'].forEach((vout: object | null) => {
 
-                                // look for scriptPubKey
-                                if (vout.hasOwnProperty('scriptPubKey')) {
-                                  if (vout['scriptPubKey']['asm'] && vout['scriptPubKey']['asm'].substr(0, 9) == 'OP_RETURN') {
-
-                                    console.log('tx '+index+' -> ', value);
-                                    console.log(vout['scriptPubKey']['asm'])
-                                    console.log('dehexed -> ', this.hex2a(vout['scriptPubKey']['asm'].substr(10, vout['scriptPubKey']['asm'].length)))
-
-                                    // attempt to create buffer for gunzip
-                                    let buff = Buffer.from(vout['scriptPubKey']['asm'].substr(10, vout['scriptPubKey']['asm'].length), 'hex');
-                                    // add to array
-                                    concatOpReturns.push(buff);
+                          if (vout.hasOwnProperty('value') && vout['value'] == 0) {
 
 
-                                  } // end test for OP_RETURN
-                                } // end look for scriptPubKey
+                            // look for scriptPubKey
+                            if (vout.hasOwnProperty('scriptPubKey')) {
+                              if (vout['scriptPubKey']['asm'] && vout['scriptPubKey']['asm'].substr(0, 9) == 'OP_RETURN') {
+
+                                let dehexed = this.hex2a(vout['scriptPubKey']['asm'].substr(10, vout['scriptPubKey']['asm'].length))
+
+                                if (dehexed.substr(0, 4) == 'S08k' || dehexed.substr(0, 3) == '08k') {
+                                  console.log(result['blocktime'])
+                                  let date = new Date(result['blocktime'] * 1000).toLocaleString("en-US");
+                                  console.log(date)
+                                  console.log(dehexed)
                                 }
-                            }); // end foreach
-                            
-                            console.log('concat -> ', concatOpReturns)
 
-                            let buffer: any;
-                            if(concatOpReturns.length > 0) {
-                              // concat buffer
-                              buffer = Buffer.concat(concatOpReturns);
-                              // gunzip buffer
-                              this.decodeOpReturn(buffer);
-                            }
+                                // attempt to create buffer for gunzip
+                                let buff = Buffer.from(vout['scriptPubKey']['asm'].substr(10, vout['scriptPubKey']['asm'].length), 'hex');
+                                // add to array
+                                concatOpReturns.push(buff);
 
-                            resolve(true);
 
-                          } // end test array before loop
+                              } // end test for OP_RETURN
+                            } // end look for scriptPubKey
+                          }
+                        }); // end foreach
 
-                        })
-                        .catch(error => {
-                          console.log(error);
-                        });
+                        //console.log('concat -> ', concatOpReturns)
+
+                        let buffer: any;
+                        if (concatOpReturns.length > 0) {
+                          // concat buffer
+                          buffer = Buffer.concat(concatOpReturns);
+                          // gunzip buffer
+                          this.decodeOpReturn(buffer, i);
+                        }
+
+                        resolve(true);
+
+                      } // end test array before loop
+
+                    })
+                    .catch(error => {
+                      console.log(error);
                     });
+                });
 
-                  } // end testing tx
-                } // end test if array keys exist
+              } // end testing tx
+            } // end test if array keys exist
 
-              })
-              .catch(error => {
-                console.log(error);
-              })
+          })
+          .catch(error => {
+            console.log(error);
+          })
 
-          } // end loop 1
+      } // end loop 1
 
     })
-    .catch(error => {
-      console.log(error);
-    }); // end promise
+      .catch(error => {
+        console.log(error);
+      }); // end promise
   }
 
-  private decodeOpReturn(buffer: any) {
+  private decodeOpReturn(buffer: any, i: number) {
 
-    zlib.gunzip(buffer, (err, dezipped) => {
-      console.log(err)
-      if(!err && dezipped) {
-        console.log(dezipped.toString('utf8'));
-      }
-      
-    });
+    try {
+
+      // need to add test to avoid zip-bombs
+      zlib.gunzip(buffer, (err, dezipped) => {
+        //console.log(err)
+        if (!err && dezipped) {
+          console.log("\nheight -> ", i)
+          console.log(dezipped.toString('utf8'));
+        }
+      })
+
+    } catch (error) {
+      //console.log(error)
+    }
+
+
+    // });
 
     //  const zipFile = yauzl.fromBuffer(buffer, { lazyEntries: true }, (err, zipfile) => {
-    //   console.log(err)
-    //   console.log(zipFile)
+    //   if(!err) {
+    //     console.log(zipFile)
+    //   }
+
     //  });
-     
-   // console.log("\n buffer concate -> ", buffer)
+
+    // console.log("\n buffer concate -> ", buffer)
     // zlib.gunzip(buffer, (err, dezipped) => {
     //   console.log('error -> ', err)
     //   //console.log(dezipped);
@@ -226,12 +234,12 @@ export class OpScanner {
     let hex = hexx.toString();
     let str = '';
     for (let i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+      str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
     return str;
   }
 
   // ------------------------------- Promises Wrapper to avoid pyramid of doom -------------------------------
-  
+
   /**
    * Get blockcount
    */
